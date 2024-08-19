396e15932e14915ba7d2f2bc04cc77185cc53370
JD-MBP <jashansingh29@gmail.com>
2024-08-19 03:57:15 -0400

    l

--- T01Menu/static/updateHeadings.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".category_heading").forEach(element => {
-    element.className = `text-red-600 font-title text-center text-7xl font-black leading-[1.1] my-4 mt-[100px]`
-});
-
-document.getElementById("Drink").className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 px-10"
-
-document.getElementById("Sides").className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 px-10"
--- T01Menu/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('',homepage,name="homepage" ),
-    path("menu/",Menu.as_view(),name="menu"),
-    path("srs/",srs_doc,name='srs'),
-    path("bp/",bp_plan,name="bp")
-]
